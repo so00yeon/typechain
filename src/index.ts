b75c611eaa56f5d9849cb3333ecbd32ae30279b8
so00yeon <96031597+so00yeon@users.noreply.github.com>
2022-02-07 00:59:30 +0900

    watch good

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ const name = "Soooo",
     gender = "male";
 
 const sayHi = (name: string, age: number, gender: string): string => {
-    return `Hello ${name}, you are ${age}, you are a ${gender}`;
+    return `Hello ${name}, you are ${age}, you are a ${gender}!`;
 };
 
 console.log(sayHi(name, age, gender));
