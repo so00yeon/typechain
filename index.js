52c30bac1574e34ed544b0379db7c699c47ffb0f
so00yeon <96031597+so00yeon@users.noreply.github.com>
2022-02-07 00:18:12 +0900

    tsc-watch only install

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "Soooo", age = 24, gender = "male";
+const sayHi = (name, age, gender) => {
+    return `Hello ${name}, you are ${age}, you are a ${gender}`;
+};
+console.log(sayHi(name, age, gender));
+//# sourceMappingURL=index.js.map
---
+++ index.ts
@@ -0,0 +1,11 @@
+const name = "Soooo",
+    age = 24,
+    gender = "male";
+
+const sayHi = (name: string, age: number, gender: string): string => {
+    return `Hello ${name}, you are ${age}, you are a ${gender}`;
+};
+
+console.log(sayHi(name, age, gender));
+
+export {};
